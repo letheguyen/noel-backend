10909c40f117fa6f7d01182f04379691d91ebc69
Le The Nguyen <nguyenlt@relipasoft.com>
2025-12-09 20:29:01 +0700

    feat: fix module error

--- src/system/system.module.ts
+++ src/system/system.module.ts
@@ -3,9 +3,10 @@ import { SystemService } from "./system.service";
 import { SystemController } from "./system.controller";
 import { RedisModule } from "../redis/redis.module";
 import { AuthModule } from "../auth/auth.module";
+import { MembersModule } from "../members/members.module";
 
 @Module({
-  imports: [RedisModule, AuthModule],
+  imports: [RedisModule, AuthModule, MembersModule],
   providers: [SystemService],
   controllers: [SystemController],
   exports: [SystemService],
